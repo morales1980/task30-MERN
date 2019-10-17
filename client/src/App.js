9d06fbefe9be2e99f9ef9ebb0bba122e4b4dd2ce
Darek.Szotowicz <dariusz.szotowicz@protonmail.com>
2019-10-17 23:23:07 +0200

    project ready to deveolp - minor update

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    posts: [],
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8000/api/posts')
+      .then(response => response.json())
+      .then(response => {
+        this.setState({ posts: response});
+      });
+  }
+
   render() {
     return (
       <div>
-        Hello world!
+        <ul>
+          {this.state.posts.map(post => <li key = {post.id}>{post.title}</li>)}
+        </ul>
       </div>
     );
   }
